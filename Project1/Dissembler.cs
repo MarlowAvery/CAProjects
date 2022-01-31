6dfda3969032a8f635f859de6c1493b01ac57cfa
MarlowAvery <marlowas@etsu.edu>
2022-01-30 21:16:41 -0500

    first draft for dictionary

---
+++ Project1/Dissembler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace CAProj1
+{
+
+    class Dissembler
+    {
+        public void InitializeDictionary()
+        {
+            Dictionary<byte, string> _instructions = new Dictionary<byte, string>
+            {
+                { 0xC0, "LWD" },
+                { 0x24, "CALL" },
+                { 0xE9, "STWX" },
+                { 0xE2, "STWA" },
+                { 0x62, "ADDA" },
+                { 0x49, "STRO" },
+                { 0x00, "STOP" }
+            };
+        }
+    
+      
+
+
+
+
+    }
+}
