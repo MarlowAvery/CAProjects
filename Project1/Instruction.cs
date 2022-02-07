e95dc8b81c105ae09ed4ce299bc3af7f459d0e22
MarlowAvery <marlowas@etsu.edu>
2022-02-07 00:05:03 -0500

    Output available

---
+++ Project1/Instruction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAProj1
+{
+    public struct Instruction
+    {
+        public Instruction(byte command, byte offsetH, byte offsetL)
+        {
+            Command = command;
+            OffsetH = offsetH;
+            OffsetL = offsetL;
+        }
+        public byte Command { get; set; }
+        public byte OffsetH { get; set; }
+        public byte OffsetL { get; set; }
+        public string Verbose()
+        {
+            String result;
+            String p1 = Command.ToString();
+            String p2 = OffsetH.ToString()+OffsetL.ToString();
+            String p3 = Dissembler.MatchKey(Command);
+            result = p1+"\t"+p3+"\t"+p2;
+            return result;
+        }
+    }
+}
--- Project1/Main.cs
+++ Project1/Main.cs
@@ -9,13 +9,26 @@ namespace CAProj1
         //main entry point
         static void Main(string[] args)
         {
-            var output = "";
+            List<Instruction> commands = new List<Instruction>(); 
+            Instruction myCommand;
             string instructions = System.IO.File.ReadAllText(@"hex.txt");
             String[] array = instructions.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
             List<Byte> vals = array.Select(arg => Byte.Parse(arg.ToString(), System.Globalization.NumberStyles.HexNumber)).ToList();
-            if(vals[0] == 0xC0)
+            for(int i = 0; i<vals.Count;i+=3)
             {
-                output = Dissembler.MatchKey(vals[0]);
+                if(vals[i]==0x00)
+                {
+                    myCommand = new Instruction(0x00, 0x00, 0x00);
+                    commands.Add(myCommand);
+                    break;
+                }
+                myCommand = new Instruction(vals[i], vals[i + 1], vals[i + 2]);
+                commands.Add(myCommand);
+                
+            }
+            for(int i = 0; i< commands.Count;i++)
+            {
+                Console.WriteLine(commands[i].Verbose());
             }
             Console.WriteLine(instructions);
         }
